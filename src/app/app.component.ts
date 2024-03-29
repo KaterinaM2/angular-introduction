548d5aa02d2233c30ef6accfc2bc821534ee1b7d
Katerina M <mavria.katerina@gmail.com>
2024-03-29 19:30:08 +0200

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,19 +14,94 @@ import { Person } from './shared/interfaces/person';
 export class AppComponent {
   name = 'Katerina';
 
-  person0: {
+  person0: Person = {
     givenName:'Katerina',
     surName:'Mavria',
     age:27,
     email: 'mavria.katerina@gmail.com',
+    address: 'Greece',
   };
 
-  person1: {
+  person1: Person= {
       givenName:'Elina',
       surName:'Wells',
       age:29,
       email: 'wellinghton@gmail.com',
+      address: 'London'
     };
+
+    users: Person[] = [
+      {
+        givenName: 'John',
+        surName: 'Doe',
+        age: 30,
+        email: 'john.doe@example.com',
+        address: '123 Main St',
+      },
+      {
+        givenName: 'Jane',
+        surName: 'Doe',
+        age: 28,
+        email: 'jane.doe@example.com',
+        address: '123 Main St',
+      },
+      {
+        givenName: 'Jim',
+        surName: 'Brown',
+        age: 45,
+        email: 'jim.brown@example.com',
+        address: '456 Park Ave',
+      },
+      {
+        givenName: 'Jill',
+        surName: 'Brown',
+        age: 42,
+        email: 'jill.brown@example.com',
+        address: '456 Park Ave',
+      },
+      {
+        givenName: 'Jake',
+        surName: 'Smith',
+        age: 36,
+        email: 'jake.smith@example.com',
+        address: '789 Broadway',
+      },
+      {
+        givenName: 'Judy',
+        surName: 'Smith',
+        age: 34,
+        email: 'judy.smith@example.com',
+        address: '789 Broadway',
+      },
+      {
+        givenName: 'Jack',
+        surName: 'Johnson',
+        age: 50,
+        email: 'jack.johnson@example.com',
+        address: '321 Oak St',
+      },
+      {
+        givenName: 'Julie',
+        surName: 'Johnson',
+        age: 48,
+        email: 'julie.johnson@example.com',
+        address: '321 Oak St',
+      },
+      {
+        givenName: 'Jerry',
+        surName: 'Davis',
+        age: 55,
+        email: 'jerry.davis@example.com',
+        address: '654 Pine St',
+      },
+      {
+        givenName: 'June',
+        surName: 'Davis',
+        age: 53,
+        email: 'june.davis@example.com',
+        address: '654 Pine St',
+      },
+    ];
   
 
   
--- src/app/components/person-table/person-table.component.ts
+++ src/app/components/person-table/person-table.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Person } from 'src/app/shared/interfaces/person.ts';
+import { Person } from '../../shared/interfaces/person';
 
 
 @Component({
