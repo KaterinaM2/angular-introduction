cc0e3aa0f54fd0d6bbfa70b81b81de021834f626
Katerina M <mavria.katerina@gmail.com>
2024-03-29 19:18:30 +0200

    input component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,33 @@
 import { Component } from '@angular/core';
 import { PersonTableComponent } from './components/person-table/person-table.component';
+import { Person } from './shared/interfaces/person';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [PersonTableComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
+
 export class AppComponent {
   name = 'Katerina';
+
+  person0: {
+    givenName:'Katerina',
+    surName:'Mavria',
+    age:27,
+    email: 'mavria.katerina@gmail.com',
+  };
+
+  person1: {
+      givenName:'Elina',
+      surName:'Wells',
+      age:29,
+      email: 'wellinghton@gmail.com',
+    };
+  
 
   
 }
